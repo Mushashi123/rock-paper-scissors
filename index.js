01088aa13109a8457c8ececac8878a9813f0297b
Sovit <tsovit729@gmail.com>
2023-12-12 20:26:23 +0545

    added a function that returns computer's choice

---
+++ index.js
@@ -0,0 +1,15 @@
+function getComputersChoice() {
+  // returns random number between 0 - 3
+  let choice = Math.trunc(Math.random() * 3);
+
+  //   each number between 0 - 3 is mapped with rock paper and scissor
+  //   the random number determines which options gets returned
+  switch (choice) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissor";
+  }
+}
